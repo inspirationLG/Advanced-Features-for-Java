be4e265e5cbd8a1cc36e57205b7580af399d79c7
inspirationLG <1185379947@qq.com>
2019-08-12 18:44:58 +0800

    类型通配符

---
+++ src/com/generics/GenericTest.java
@@ -0,0 +1,30 @@
+package com.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author:Hchien Ying
+ * @date:2019/8/12
+ * @description:
+ */
+public class GenericTest {
+    public static void main(String[] args) {
+        List<String> name = new ArrayList<String>();
+        List<Integer> age = new ArrayList<Integer>();
+        List<Number> number = new ArrayList<Number>();
+
+        name.add("icon");
+        age.add(18);
+        number.add(314);
+
+        getData(name);
+        getData(age);
+        getData(number);
+
+    }
+
+    public static void getData(List<?> data) {
+        System.out.println("data:" + data.get(0));
+    }
+}
