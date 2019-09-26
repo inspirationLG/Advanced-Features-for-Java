b76bd17a9c29273b50c837fe1e5f0409250b8e89
hchienisabad <hchienisabad@gmail.com>
2019-09-26 21:47:08 +0800

    面向对象类的构造函数、set、get

---
+++ src/com/OOP/parameterExc1.java
@@ -0,0 +1,33 @@
+/*
+package com.OOP;
+
+*/
+/**
+ * @Author:Hchien Ying
+ * @date:2019/9/26
+ * @description:
+ *//*
+
+public class parameterExc1 {
+    public static void main(String[] args) {
+        Person p = new Person();
+        int n = 15; // n的值为15
+        p.setAge(n); // 传入n的值
+        System.out.println(p.getAge()); // 15
+        n = 20; // n的值改为20
+        System.out.println(p.getAge()); // 15还是20?
+    }
+}
+class Person {
+    private int age;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+*/
