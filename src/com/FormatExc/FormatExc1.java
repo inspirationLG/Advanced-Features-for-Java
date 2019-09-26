026d91affe59075826d44901aa9e6495519b31bb
hchienisabad <hchienisabad@gmail.com>
2019-09-26 19:44:02 +0800

    format test1

---
+++ src/com/FormatExc/FormatExc1.java
@@ -0,0 +1,14 @@
+package com.FormatExc;
+
+/**
+ * @Author:Hchien Ying
+ * @date:2019/9/26
+ * @description:
+ */
+public class FormatExc1 {
+    public static void main(String[] args) {
+        double d = 3.1415926;
+        System.out.printf("%.2f\n", d); // 显示两位小数3.14
+        System.out.printf("%.4f\n", d); // 显示4位小数3.1416
+    }
+}
