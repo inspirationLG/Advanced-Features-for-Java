137aea4d23d9e91e871a015df649a762ba4d3a39
hchienisabad <hchienisabad@gmail.com>
2019-09-26 21:18:53 +0800

    数组

---
+++ src/com/ArrayExc/ArrayExc2.java
@@ -0,0 +1,14 @@
+package com.ArrayExc;
+
+import java.util.Arrays;
+
+/**
+ * @Author:Hchien Ying
+ * @date:2019/9/26
+ * @description:
+ */
+public class ArrayExc2 {
+    public static void main(String[] args) {
+        int[] ns = { 1, 1, 2, 3, 5, 8 };
+        System.out.println(Arrays.toString(ns));    }
+}
