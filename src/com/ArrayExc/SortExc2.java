57761230b09de9719ed1d1ab82de5879f40a5588
hchienisabad <hchienisabad@gmail.com>
2019-09-26 21:15:18 +0800

    二维数组

---
+++ src/com/ArrayExc/SortExc2.java
@@ -0,0 +1,19 @@
+package com.ArrayExc;
+
+import java.util.Arrays;
+
+/**
+ * @Author:Hchien Ying
+ * @date:2019/9/26
+ * @description:
+ */
+public class SortExc2 {
+    public static void main(String[] args) {
+        int[][] ns = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 }
+        };
+        System.out.println(Arrays.deepToString(ns));
+    }
+}
