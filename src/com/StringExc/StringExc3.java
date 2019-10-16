77235390f6bd8d7de94a41a7d79fee3001631de3
hchienisabad <hchienisabad@gmail.com>
2019-10-16 21:40:31 +0800

    2

---
+++ src/com/StringExc/StringExc3.java
@@ -0,0 +1,15 @@
+package com.StringExc;
+
+/**
+ * @Author:Hchien Ying
+ * @date:2019/10/16
+ * @description:
+ */
+public class StringExc3 {
+    public static void main(String[] args) {
+        String s = "hello";
+        System.out.println(s);
+        s = s.toUpperCase();//全部改为大写
+        System.out.println(s);
+    }
+}
