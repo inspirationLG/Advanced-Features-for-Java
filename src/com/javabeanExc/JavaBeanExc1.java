1ef6a6c9504680a1599bce2a666c501c1d73000d
hchienisabad <hchienisabad@gmail.com>
2019-10-17 21:18:31 +0800

    2

--- src/com/javabeanExc/JavaBeanExc1.java
+++ src/com/javabeanExc/JavaBeanExc1.java
@@ -20,7 +20,7 @@ public class JavaBeanExc1 {public static void main(String[] args) throws Excepti
 }
 class Person {
     private String name;
-    private int age;
+    private int age;22
 
     public String getName() {
         return name;
